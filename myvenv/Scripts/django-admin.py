0f64d975758bb9a13c420d3fa9c902d3dda8b3c7
choihyejeong010201 <anna010201@naver.com>
2020-05-02 12:05:48 +0900

    final

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\82104\desktop\서강\3학기\멋쟁이 사자처럼\20200413\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
